33a2ec83f6ba2116cb08e2538145ba7b64b6f9a1
Taeseok Ryu <yuteta@gmail.com>
2020-07-28 00:07:45 +0900

    change career list

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,6 @@ const IndexPage = () => (
       <h2 className="section-title">Career<span className="comma">,</span></h2>
       <ul className="list-career">
         <li className="item">
-          <h3 className="item-title"><a href="https://trazy.com" target="_blank" rel="noopener noreferrer">트레이지</a></h3>
-          <p className="item-excerpt">웹 디자이너 / 2012.02 ~ 2014.01</p>
-        </li>
-        <li className="item">
           <h3 className="item-title"><a href="https://slowalk.co.kr" target="_blank" rel="noopener noreferrer">슬로워크</a></h3>
           <p className="item-excerpt">프론트엔드 개발자 / 2014.02 ~ 2019.03</p>
         </li>
@@ -52,24 +48,8 @@ const IndexPage = () => (
           <p className="item-excerpt">앱 프론트엔드 개발, PM 대리 / native base 커스터마이즈 개발 / react native, native base</p>
         </li>
         <li className="item">
-          <h3 className="item-title"><a href="https://www.neosherlock.com/" target="_blank" rel="noopener noreferrer">셜록프레스</a></h3>
-          <p className="item-excerpt">PM, 퍼블리싱, 워드프레스 개발  / 후원신청폼 개발 / wordpress, less</p>
-        </li>
-        <li className="item">
           <h3 className="item-title"><a href="http://www.beautifulstore.org/" target="_blank" rel="noopener noreferrer">아름다운가게 통합사이트</a></h3>
           <p className="item-excerpt">퍼블리싱, 워드프레스 개발 / 워드프레스 멀티사이트 개발, 기증신청폼 유효성검사 및 인터랙션 개발 / wordpress</p>
-        </li>
-        <li className="item">
-          <h3 className="item-title"><a href="https://purme.org/" target="_blank" rel="noopener noreferrer">푸르메재단</a></h3>
-          <p className="item-excerpt">퍼블리싱, 워드프레스 개발 / 주요 아카이브 개발, 접근성 대응 / wordpress, less</p>
-        </li>
-        <li className="item">
-          <h3 className="item-title"><a href="https://maru180.com/" target="_blank" rel="noopener noreferrer">마루180</a></h3>
-          <p className="item-excerpt">전체 개발 / 회원 이벤트 캘린더 개발, 미팅룸 캘린더 개발 / wordpress, less</p>
-        </li>
-        <li className="item">
-          <h3 className="item-title"><a href="https://www.baconbox.co/" target="_blank" rel="noopener noreferrer">베이컨박스</a></h3>
-          <p className="item-excerpt">프론트엔드 개발 / 구독 페이지 단계별 인터랙션 개발 / wordpress, woocommerce, less</p>
         </li>
         <li className="item">
           <h3 className="item-title"><a href="http://realmania.net/euro2016/" target="_blank" rel="noopener noreferrer">레알매니아 유로2016</a> &amp; <a href="http://realmania.net/awards/1516/" target="_blank" rel="noopener noreferrer">1516 시즌 어워드페이지</a></h3>
