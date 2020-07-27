65cf3e7651343cba3fdcb9b75de0c78a747c5360
Taeseok Ryu <yuteta@gmail.com>
2020-07-28 01:31:03 +0900

    Updates

---
+++ component---src-pages-404-js-03b305a101ccc092d573.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"페이지가 존재하지 않습니다."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-03b305a101ccc092d573.js.map
